5c47d1c44250db206ad8bb4486611652311ded1e
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-12-05 22:18:29 -0300

    Initial Commit

---
+++ Fruts/FrutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutsApp.swift
+//  Fruts
+//
+//  Created by kenjimaeda on 05/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FrutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
